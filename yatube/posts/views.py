2d50aa46560367e981f16048bea57c4ebee25c76
Айдар Нигамадьянов <angelof.wars@yandex.ru>
2022-10-20 15:33:50 +0300

    ÐСделал бÐэйс и раставил шаблоны

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,7 +2,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 # Главная страница
 def index(request):
-    return render(request, 'posts/index.html')
+    title = 'Yatube - соцсеть для разработчиков'
+    context = {
+        'title': title,
+        # А можно сразу записать значение в словарь. Но обычно так не делают
+        'text': 'Главная страница',
+    }
+    return render(request, 'posts/index.html', context)
 
 
 
