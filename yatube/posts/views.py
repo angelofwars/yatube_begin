2fd5427b057f190fdca188be4e02e7b826acdad9
Айдар Нигамадьянов <angelof.wars@yandex.ru>
2022-10-24 22:44:05 +0300

    Все работает сделал некоторые изменÐ коÐкомерÑкосметические изменения

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 # Импортируем модель, чтобы обратиться к ней
-from .models import Post
+from .models import Post, Group
+
 
 
 # Главная страница
@@ -14,7 +15,15 @@ def index(request):
     return render(request, 'posts/index.html', context)
 
 
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = group.posts.all()[:10]
+    title = 'Гроуп-пост - еее бой'
+    context = {
+        'group': group,
+        'posts': posts,
+        'title': title,
+    }
+    return render(request, 'posts/group_list.html', context)
 
 # view-функция принимает параметр slug из path()
-def group_posts(request, slug):
-    return render(request, 'posts/group_list.html')
